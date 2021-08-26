c388441d7184f3acc096b84d21e44c1ea6cca217
Vipul Kumar Singh <vipulrjput@gmail.com>
2021-08-26 07:35:36 +0530

    Added LARGEST NO.

---
+++ largest_no.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std ;
+
+int main(void )
+{
+    cout <<"ENter the no.s for comparison " ;
+
+    int n1 , n2 ;
+
+    cin >>n1 >> n2 ;
+    if (n1 > n2) cout <<"the largest no. is " <<n1 ;
+
+    else cout <<"the largest no. is " <<n2 ;
+
+    return  0 ;
+}
