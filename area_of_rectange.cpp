ceb3da3f923798aec81b613392ec14f07d323474
Vipul Kumar Singh <vipulrjput@gmail.com>
2021-08-26 09:29:14 +0530

    area of rectangle

--- area_of_rectange.cpp
+++ area_of_rectange.cpp
@@ -5,7 +5,7 @@ using namespace std ;
 
 int main (void )
 {
-    cout <<"enter the length and breadth of rectangle \n"<<endl;
+    cout <<"enter the length and breadth of rectangle "<<endl;
     int len , br ;
 
     cin >>len >>br ;
