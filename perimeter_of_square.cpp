30c28be2f9b7a2a974b5fa09e306493caca87721
Vipul Kumar Singh <vipulrjput@gmail.com>
2021-08-26 14:52:25 +0530

    perimeter of square

---
+++ perimeter_of_square.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std ;
+
+int main(void )
+{
+    cout <<"Enter the perimeter of square ";
+
+    int len ;
+
+    cin >>len ;
+
+
+    cout <<"perimeter of square "<<4 *len ;
+    
+    return 0;
+}
