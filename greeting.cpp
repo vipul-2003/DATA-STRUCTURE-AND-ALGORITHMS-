cb41e8ca0c53da8c6e7d4cab465890afdfe0681c
Vipul Kumar Singh <vipulrjput@gmail.com>
2021-08-26 06:59:37 +0530

    added greeting function

---
+++ greeting.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std ;
+
+int main (void )
+{
+    cout <<"enter the name "<<endl;
+    string name ;
+
+    getline (cin , name );
+
+    cout <<"hi!! "<<name <<"\n";
+
+
+    return  0;
+
+
+}
