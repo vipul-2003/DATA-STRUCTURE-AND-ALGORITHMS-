72cd8e6bb537d75498bd977ca1374a406e02d9b2
Vipul Kumar Singh <vipulrjput@gmail.com>
2021-08-27 08:04:46 +0530

    Calculate compound interest Program

---
+++ calculating_compound_interest.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+
+using namespace std;
+
+int main(void )
+{
+
+    cout <<"Calculating Compound Interest \n";
+    cout <<"Enter the principle amount "<<endl;
+    int principle ;
+    cin>> principle;
+
+    cout <<"Enter the interest rate "<<endl;
+    float interestRate ;
+    cin>> interestRate;
+
+    cout<<"Enter the no. of times interest applied per time period"<<endl;
+    float n;
+    cin>>n;
+
+    cout <<"Enter the no. of time periods elapsed "<<endl;
+    float time;
+    cin >> time ;
+
+
+    float amount = principle*pow((1 + (interestRate/n)) , n*time );
+
+    cout <<"Compound Interest is " << amount <<endl;
+    
+
+    return 0;
+}
