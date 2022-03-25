e9e31336a6fe4abd2b0b0b67a7cb3ef07e7192e4
Vipul Kumar Singh <Vipulrjput@gmail.com>
2022-03-25 13:28:56 +0530

    Added file for linklist using class

--- linklist/linklist_using_class.cpp
+++ linklist/linklist_using_class.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file linklist_using_class.cpp
+ * @author Vipul Kumar Singh
+ * @brief linklist is made with help of class and constructor
+ * @version 0.1
+ * @date 2022-03-25
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #include <iostream>
 #include <stdio.h>
 
@@ -22,13 +32,14 @@ public:
 };
 
 // to insert the element at tail
-void inserAtTail(Linklist* &head, int data, string naam)
+void inserAtTail(Linklist *&head, int data, string naam)
 {
     Linklist *new_node = new Linklist(data, naam);
     if (head == NULL)
     {
         head = new_node;
-        // and new_node should have Null by paramaetrized constructor
+        // and new_node should have Null by paramaetrized cosnstructor
+        return;
     }
 
     Linklist *temp = head;
@@ -47,9 +58,8 @@ void insertAtHead(Linklist *&head, int data, string naam)
 {
     Linklist *new_node = new Linklist(data, naam);
 
-
     new_node->next = head;
-      head = new_node;
+    head = new_node;
 }
 
 // to display the elements of the Linklist
@@ -58,10 +68,10 @@ void display(Linklist *head)
     Linklist *temp = head;
     while (temp != NULL)
     {
-        cout << "Name " << temp->name << "roll no. " << temp->rollno << " --> ";
+        cout << "Name =" << temp->name << " roll no. =" << temp->rollno << " --> ";
         temp = temp->next;
     }
-    cout << endl;
+    cout << "NULL " << endl;
 }
 
 int main()
@@ -71,6 +81,12 @@ int main()
     inserAtTail(head, 1, "vipul kumar");
     inserAtTail(head, 2, "Aman kumar");
     inserAtTail(head, 3, "Vipul kumar Singh");
+
+    display(head);
+
+    insertAtHead(head, 4, "Ram Kumar ");
+
+    insertAtHead(head, 5, " shri Rajesh Kumar ");
 
     display(head);
 
