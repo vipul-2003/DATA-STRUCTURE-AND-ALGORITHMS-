63a0e4b2b3ebbd01a505090e8df78cc7b8b286ee
Vipul Kumar Singh <Vipulrjput@gmail.com>
2022-03-28 08:32:43 +0530

    --- Auto-Git Commit ---

---
+++ linklist/reverse_linklist.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+class Linklist
+{
+public:
+    int rollno;
+    string name;
+
+    Linklist(int val, string naam)
+    {
+        rollno = val;
+        name = naam;
+    }
+
+    
+};
