4231143cda859dfc4717ecd7395d8862be1c3e41
Vipul Kumar Singh <Vipulrjput@gmail.com>
2022-05-01 12:28:35 +0530

    --- Auto-Git Commit ---

--- linklist/doubly_linklist.cpp
+++ linklist/doubly_linklist.cpp
@@ -20,19 +20,70 @@ public:
     }
 };
 
-void insertAtHead(node *head, int val)
+void insertAtHead(node *&head, int val)
 {
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+
+        return;
+    }
+
+    n->next = head;
+    head->prev = n;
+
+    head = n;
 }
 
-void insertAtTail(node *head, int val)
+void insertAtTail(node *&head, int val)
 {
-    if (head ==  NULL)
+
+    if (head == NULL)
     {
-        insertAtHead(head , val);
+        insertAtHead(head, val);
+        return;
     }
+
+    node *n = new node(val);
+
+    node *temp = head;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = n;
+    n->prev = temp;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL " << endl;
+
+    cout << "\n";
 }
 
 int main()
 {
+
+    node *head = NULL;
+
+    insertAtHead(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+    insertAtHead(head, 0);
+
+    display(head);
     return 0;
 }
