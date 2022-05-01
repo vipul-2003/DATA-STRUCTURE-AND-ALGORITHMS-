36abf26596c0fd6ccc3afb8c0565eb635c64bef7
Vipul Kumar Singh <Vipulrjput@gmail.com>
2022-05-01 12:46:25 +0530

    pushed code

--- linklist/doubly_linklist.cpp
+++ linklist/doubly_linklist.cpp
@@ -58,6 +58,33 @@ void insertAtTail(node *&head, int val)
     n->prev = temp;
 }
 
+void deleteAtHead(node *&head)
+{
+    node *todelete = head;
+    head = head->next;
+    delete (todelete);
+}
+
+void deleteAtTail(node *&head)
+{
+    node *temp = head;
+
+    if (head == NULL)
+    {
+        cout << "Linklist is empty" << endl;
+    }
+
+    while (temp->next->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    node *todelete = temp->next;
+    temp->next = NULL;
+
+    delete (todelete);
+}
+
 void display(node *head)
 {
     node *temp = head;
@@ -72,6 +99,26 @@ void display(node *head)
     cout << "\n";
 }
 
+void deleteByValue(node *&head, int del)
+{
+    node *temp = head;
+
+    while (temp->next != NULL)
+    {
+        if (temp->data == del)
+        {
+            cout << "Value is found and deleted" << endl;
+            node *todelete = temp;
+            temp->prev->next = temp->next;
+
+            delete (todelete);
+
+            return;
+        }
+        temp = temp->next;
+    }
+}
+
 int main()
 {
 
@@ -83,6 +130,10 @@ int main()
     insertAtTail(head, 4);
     insertAtTail(head, 5);
     insertAtHead(head, 0);
+
+    deleteAtHead(head);
+    deleteAtTail(head);
+    deleteByValue(head, 2);
 
     display(head);
     return 0;
